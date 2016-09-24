200664b77eaa5e8beb90cfa1e85bff78c0be3132
Andrew Pogrebnoy <absourd.noise@gmail.com>
2016-09-25 01:59:54 +0300

    SSA presentation

---
+++ kiev.meetup.sep.2016/ssa-intr1.go
@@ -0,0 +1,19 @@
+package slides
+
+func ex1() {
+	// START OMIT
+	x := 1
+	y := 2
+	x = 3
+	y = x + 4
+	// END OMIT
+}
+
+func ex2() {
+	// START2 OMIT
+	x1 := 1
+	y1 := 2
+	x2 := 3
+	y2 := x2 + 4
+	// END2 OMIT
+}
